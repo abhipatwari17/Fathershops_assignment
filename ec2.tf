d02848030981bfe7682e34216526354347b3c33d
abhipatwari17 <88781436+abhipatwari17@users.noreply.github.com>
2023-12-02 14:15:59 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,31 +1,29 @@
 # ECS Container Instances
 
 resource "aws_instance" "ecs-instance01" {
-    ami                         = "${lookup(var.amis, var.region)}"
-    instance_type               = "${var.instance_type}"
+    ami                         = lookup(var.amis, var.region)
+    instance_type               = var.instance_type
     availability_zone           = "us-west-2a"
-    subnet_id                   = "${aws_subnet.wp-public-tf.id}"
-    key_name                    = "${var.key_name}"
+    subnet_id                   = aws_subnet.wp-public-tf.id
     associate_public_ip_address = true
     iam_instance_profile        = "ecsInstanceRole"
     security_groups             = ["${aws_security_group.wp-ecs-sg-tf.id}"]
     user_data                   = "#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.default.name} > /etc/ecs/ecs.config"
-    tags {
+    tags = {
       Name = "ecs-instance01"
     }
 }
 
 resource "aws_instance" "ecs-instance02" {
-    ami                         = "${lookup(var.amis, var.region)}"
-    instance_type               = "${var.instance_type}"
+    ami                         = lookup(var.amis, var.region)
+    instance_type               = var.instance_type
     availability_zone           = "us-west-2a"
-    subnet_id                   = "${aws_subnet.wp-public-tf.id}"
-    key_name                    = "${var.key_name}"
+    subnet_id                   = aws_subnet.wp-public-tf.id
     associate_public_ip_address = true
     iam_instance_profile        = "ecsInstanceRole"
     security_groups             = ["${aws_security_group.wp-ecs-sg-tf.id}"]
     user_data                   = "#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.default.name} > /etc/ecs/ecs.config"
-    tags {
+    tags = {
       Name = "ecs-instance02"
     }
 }
