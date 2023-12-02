3b9ad62bea6caba131f3a866b77f5de3743b1139
abhipatwari17 <88781436+abhipatwari17@users.noreply.github.com>
2023-12-02 14:21:20 +0530

    Update templates.tf

--- templates.tf
+++ templates.tf
@@ -1,14 +1,14 @@
 resource "template_file" "wp-container" {
-  template = "${file("task-definitions/wordpress.json")}"
+  template = file("task-definitions/wordpress.json")
 
-  vars {
-    db_host     = "${aws_db_instance.wordpress.endpoint}"
-    db_name     = "${var.db_name}"
-    db_user     = "${var.db_user}"
-    db_password = "${var.db_password}"
-    wp_title    = "${var.wp_title}"
-    wp_user     = "${var.wp_user}"
-    wp_password = "${var.wp_password}"
-    wp_mail     = "${var.wp_mail}"
+  vars = {
+    db_host     = replace(aws_db_instance.wordpress.endpoint, ":.*", "")  # Extract host part without port
+    db_name     = var.db_name
+    db_user     = var.db_user
+    db_password = var.db_password
+    wp_title    = var.wp_title
+    wp_user     = var.wp_user
+    wp_password = var.wp_password
+    wp_mail     = var.wp_mail
   }
 }
