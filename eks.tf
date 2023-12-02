6069b342b37c9b771f813a642145b272364b4f15
abhipatwari17 <88781436+abhipatwari17@users.noreply.github.com>
2023-12-02 14:17:33 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,8 +1,9 @@
+# EKS Cluster
 resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-eks-cluster"
+  name     = "my-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-  vpc_config {
-    subnet_ids = aws_subnet.private[*].id
+
+   vpc_config {
+   subnet_ids = [for subnet in aws_subnet.wp-private-tf : subnet.id]  # Map subnet objects to their IDs
   }
 }
-
